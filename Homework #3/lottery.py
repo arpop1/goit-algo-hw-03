6305c8d959c2fbf8815a65a159958413e06452be
arpop1 <tim.popovych.tp@gmail.com>
2025-02-24 19:03:25 +0100

    Update lottery.py

--- Homework #3/lottery.py
+++ Homework #3/lottery.py
@@ -3,17 +3,17 @@ import random
 def get_numbers_ticket(min, max, quantity):
         try:
             numbers_set = set()
-            if min >= 1 and max <= 1000:
+            limit = max - min #є ідея написати таку формулу: (max - min)+1. Уявімо що нам потрібно 5 чисел від 1 до 5 (вийде 1, 2, 3, 4, 5.), тоді формула max - min не підійде?
+            if min >= 1 and min < max <= 1000 and quantity <= limit:
                 while len(numbers_set) < quantity:
                      number = random.randrange(min, max)
                      numbers_set.add(number)
-            else:
-                print("Числа повинні бути від 1 до 1000")
             numbers_list = list(numbers_set)
             numbers_list.sort()
             return numbers_list 
         except (TypeError, ValueError, NameError):
             print("Неправильно введені дані. Використувуйте числа")
+            return numbers_list
 
-result = get_numbers_ticket(1, 5, 4)
+result = get_numbers_ticket(10, 14, 6)
 print(result) #Повертає тільки 1, 2, 3, 4, оскільки інших комбінацій бути не може
