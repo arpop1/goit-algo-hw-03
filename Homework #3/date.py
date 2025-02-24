7d9f1e9afa80ac9283dda4949417ad10513d6279
arpop1 <tim.popovych.tp@gmail.com>
2025-02-24 19:03:53 +0100

    Update date.py

--- Homework #3/date.py
+++ Homework #3/date.py
@@ -2,13 +2,13 @@ from datetime import datetime
 
 def get_days_from_today(date):
         try:
-            date_str = datetime.strptime( date, "%Y-%m-%d")
-            today = datetime.today()
+            date_str = datetime.strptime( date, "%Y-%m-%d").date()
+            today = datetime.today().date()
             difference = date_str - today 
             return difference.days
         except ValueError:
             print(f"{date} має бути у форматі YYYY-MM-DD. Спробуйте знову, використовуючи цифри")
             return None 
 
-days_difference = get_days_from_today("2021-10-09")
+days_difference = get_days_from_today("2025-02-24")
 print(f"Різниця у днях: {days_difference}")
